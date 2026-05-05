b120a0adaf544d3fecc3846d8d5cfcda0a028601
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2026-05-05 12:55:05 +0900

    Rebase from 'upstream'

--- localization/autoware_ekf_localizer/src/mahalanobis.cpp
+++ localization/autoware_ekf_localizer/src/mahalanobis.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "autoware/ekf_localizer/mahalanobis.hpp"
+#include "include/mahalanobis.hpp"
 
 namespace autoware::ekf_localizer
 {
